fa71d2d6ad77374aa552f90653e60af4717355c0
Your Name <you@example.com>
2019-03-22 12:35:31 +0200

    added form feedback

---
+++ src/app/form-feedback/form-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-feedback',
+  templateUrl: './form-feedback.component.html',
+  styleUrls: ['./form-feedback.component.scss']
+})
+export class FormFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
