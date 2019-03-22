fef6cfeae2ac84d7541adcf5c08cac68274f7525
Your Name <you@example.com>
2019-03-22 12:18:06 +0200

    added our prod component

---
+++ src/app/our-prod/our-prod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-prod',
+  templateUrl: './our-prod.component.html',
+  styleUrls: ['./our-prod.component.scss']
+})
+export class OurProdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
