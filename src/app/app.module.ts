e108c93644178c2a288dc737b4f4ce310662d0e4
Your Name <you@example.com>
2019-03-25 20:52:17 +0200

    second touch on footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { HeadSliderComponent } from "./head-slider/head-slider.component";
-import { OurProdComponent } from "./our-prod/our-prod.component";
-import { OurPartnersComponent } from "./our-partners/our-partners.component";
-import { OurMissionComponent } from "./our-mission/our-mission.component";
-import { FormCountComponent } from "./form-count/form-count.component";
-import { YoutubeComponent } from "./youtube/youtube.component";
-import { OurWorksComponent } from "./our-works/our-works.component";
-import { FormFeedbackComponent } from "./form-feedback/form-feedback.component";
-import { OurBlogComponent } from "./our-blog/our-blog.component";
-import { FooterComponent } from "./footer/footer.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeadSliderComponent } from './head-slider/head-slider.component';
+import { OurProdComponent } from './our-prod/our-prod.component';
+import { OurPartnersComponent } from './our-partners/our-partners.component';
+import { OurMissionComponent } from './our-mission/our-mission.component';
+import { FormCountComponent } from './form-count/form-count.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+import { OurWorksComponent } from './our-works/our-works.component';
+import { FormFeedbackComponent } from './form-feedback/form-feedback.component';
+import { OurBlogComponent } from './our-blog/our-blog.component';
+import { FooterComponent } from './footer/footer.component';
 
-import { SlickCarouselModule } from "ngx-slick-carousel";
-import { OurFeedbackComponent } from "./our-feedback/our-feedback.component";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { OurCertificatesComponent } from "./our-certificates/our-certificates.component";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { OurFeedbackComponent } from './our-feedback/our-feedback.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurCertificatesComponent } from './our-certificates/our-certificates.component';
 
 @NgModule({
   declarations: [
