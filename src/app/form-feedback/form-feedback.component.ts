1ef8fd5bfe8c6c5c60d4187e0912a0d252d20a9c
Your Name <you@example.com>
2019-03-30 13:11:22 +0200

    started feedback form

--- src/app/form-feedback/form-feedback.component.ts
+++ src/app/form-feedback/form-feedback.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-form-feedback',
-  templateUrl: './form-feedback.component.html',
-  styleUrls: ['./form-feedback.component.scss']
+    selector: 'app-form-feedback',
+    templateUrl: './form-feedback.component.html',
+    styleUrls: ['./form-feedback.component.scss', '../shared/styles/icons.css', '../shared/styles/style.min.css']
 })
 export class FormFeedbackComponent implements OnInit {
+    feedbackForm = new FormGroup({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+        comment: new FormControl(''),
+    });
+    // http: HttpClient;
+    endpoint = 'http://localhost/post_mail/sendMail.php';
+    constructor(private http: HttpClient) {
+    }
+    onSubmit() {
+        let postVars = {
+            email: this.feedbackForm.value.email,
+            name: this.feedbackForm.value.email,
+            message: this.feedbackForm.value.comment
+        };
 
-  constructor() { }
+        // You may also want to check the response. But again, let's keep it simple.
+        this.http.post(this.endpoint, postVars)
+            .subscribe(
+                response => console.log(response),
+                response => console.log(response)
+            );
+    }
+    ngOnInit() {
 
-  ngOnInit() {
-  }
+    }
+
 
 }
+
+
