0736bf9e4d3d21fdd42caf1783b1453bce71d286
Your Name <you@example.com>
2019-04-01 23:17:44 +0300

    fb posts and audit vulnerabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeadSliderComponent } from './head-slider/head-slider.component';
-import { OurProdComponent } from './our-prod/our-prod.component';
-import { OurPartnersComponent } from './our-partners/our-partners.component';
-import { OurMissionComponent } from './our-mission/our-mission.component';
-import { FormCountComponent } from './form-count/form-count.component';
-import { YoutubeComponent } from './youtube/youtube.component';
-import { OurWorksComponent } from './our-works/our-works.component';
-import { FormFeedbackComponent } from './form-feedback/form-feedback.component';
-import { OurBlogComponent } from './our-blog/our-blog.component';
-import { FooterComponent } from './footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HeadSliderComponent} from './head-slider/head-slider.component';
+import {OurProdComponent} from './our-prod/our-prod.component';
+import {OurPartnersComponent} from './our-partners/our-partners.component';
+import {OurMissionComponent} from './our-mission/our-mission.component';
+import {FormCountComponent} from './form-count/form-count.component';
+import {YoutubeComponent} from './youtube/youtube.component';
+import {OurWorksComponent} from './our-works/our-works.component';
+import {FormFeedbackComponent} from './form-feedback/form-feedback.component';
+import {OurBlogComponent} from './our-blog/our-blog.component';
+import {FooterComponent} from './footer/footer.component';
 
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { OurFeedbackComponent } from './our-feedback/our-feedback.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { OurCertificatesComponent } from './our-certificates/our-certificates.component';
-import { HamburgerComponent } from './header/hamburger/hamburger.component';
+import {SlickCarouselModule} from 'ngx-slick-carousel';
+import {OurFeedbackComponent} from './our-feedback/our-feedback.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {OurCertificatesComponent} from './our-certificates/our-certificates.component';
+import {HamburgerComponent} from './header/hamburger/hamburger.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { CustomSelectComponent } from './shared/custom-select.component';
-import { ClickOutsideModule } from 'ng-click-outside';
+import {CustomSelectComponent} from './shared/custom-select.component';
+import {ClickOutsideModule} from 'ng-click-outside';
+
+import {FacebookModule} from 'ngx-facebook';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HeadSliderComponent,
-    OurProdComponent,
-    OurPartnersComponent,
-    OurMissionComponent,
-    FormCountComponent,
-    YoutubeComponent,
-    OurWorksComponent,
-    FormFeedbackComponent,
-    OurBlogComponent,
-    FooterComponent,
-    OurFeedbackComponent,
-    AboutUsComponent,
-    OurCertificatesComponent,
-    HamburgerComponent,
-    CustomSelectComponent
-  ],
-  imports: [BrowserModule, SlickCarouselModule, FormsModule, ClickOutsideModule,  ReactiveFormsModule, HttpClientModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HeadSliderComponent,
+        OurProdComponent,
+        OurPartnersComponent,
+        OurMissionComponent,
+        FormCountComponent,
+        YoutubeComponent,
+        OurWorksComponent,
+        FormFeedbackComponent,
+        OurBlogComponent,
+        FooterComponent,
+        OurFeedbackComponent,
+        AboutUsComponent,
+        OurCertificatesComponent,
+        HamburgerComponent,
+        CustomSelectComponent
+    ],
+    imports: [BrowserModule, SlickCarouselModule, FormsModule, ClickOutsideModule, FacebookModule.forRoot(), ReactiveFormsModule, HttpClientModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/our-blog/our-blog.component.ts
+++ src/app/our-blog/our-blog.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FacebookService, InitParams} from 'ngx-facebook';
+
+// import {FaceBookPostsService} from '../providers/facebook-graph-service.service';
 
 @Component({
-  selector: 'app-our-blog',
-  templateUrl: './our-blog.component.html',
-  styleUrls: ['./our-blog.component.scss']
+    selector: 'app-our-blog',
+    templateUrl: './our-blog.component.html',
+    styleUrls: ['./our-blog.component.scss']
 })
 export class OurBlogComponent implements OnInit {
+    token = 'EAAFEmfVsX5oBAEBZBR5utgbqXkWF7nZCwbeI3Oh4ZAbRmmOa6kTmNK0WKI8jhsSGOXKmSSsnLtfFQp5P7KlYpDMZARZCHaK1hqe84IYLJ6hJZA7EPIMbYjgHTutUQ6y2jct6PQoNBBsscQUOZCkZBmmorKPPPriSszqfGDG49CWUQwZDZD';
+    results = [];
+    error: any;
+    facebook: any;
 
-  constructor() { }
+    constructor(private http: HttpClient, private fb: FacebookService) {
+        let initParams: InitParams = {
+            appId: '356903014915994',
+            xfbml: true,
+            version: 'v3.2'
+        };
+        fb.init(initParams);
 
-  ngOnInit() {
-  }
+    }
 
+//   FB.api(
+//   '/smartpocketsua/feed',
+//   'GET',
+// {},
+// function(response) {
+//   // Insert your code here
+// }
+// );
+//   GET https://graph.facebook.com/v2.9/LADbible/posts?access_token={ACCESS_TOKEN}
+    ngOnInit() {
+        this.fb.api('smartpocketsua/feed?limit=2&fields=permalink_url&access_token=' + this.token)
+            .then((res: any) => {
+                // console.log(res);
+                this.results = res.data;
+                console.log(this.results.length);
+            })
+            .catch(this.handleError);
+
+        // this.fb.getPosts().subscribe(
+        //     data => {
+        //         this.results = data;
+        //         console.log('values  in results');
+        //         console.log(this.results);
+        //         for (let i of this.results) {
+        //
+        //             console.log('url is ' + i.permalink_url);
+        //         }
+        //     },
+        //
+        //     error => console.log(error)
+        // );
+        // this.http.get('https://graph.facebook.com/v3.2/smartpocketsua/posts?access_token=' + this.token)
+        //     .subscribe(
+        //         response => console.log(response),
+        //         response => console.log(response)
+        //     );
+    }
+
+    private handleError(error) {
+        console.error('Error processing action', error);
+    }
 }
