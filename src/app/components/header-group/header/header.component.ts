b2d3dd34d1828da1892d00f897275f3509b8e2b3
Anastasiia Kutalova <xxttc@eg.dk>
2020-10-27 00:35:37 +0200

    new contacts

--- src/app/components/header-group/header/header.component.ts
+++ src/app/components/header-group/header/header.component.ts
@@ -3,7 +3,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
